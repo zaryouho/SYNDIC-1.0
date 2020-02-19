b637065191662c68e4db892205eec8ca819487b2
ZARYOUHO <zaryouho@gmail.com>
2020-02-19 20:44:27 +0100

    -Syndic logo changed -Résidence Button

--- SYNDIC 1.0/Forms/Form1.Designer.cs
+++ SYNDIC 1.0/Forms/Form1.Designer.cs
@@ -30,6 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuOfficiel));
             this.MenuVertical = new System.Windows.Forms.Panel();
+            this.BarreTitulaire = new System.Windows.Forms.Panel();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.btnMinimized = new System.Windows.Forms.PictureBox();
+            this.btnMaximized = new System.Windows.Forms.PictureBox();
+            this.btnRestaure = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.btnSlideBar = new System.Windows.Forms.PictureBox();
             this.buttonSettings = new System.Windows.Forms.Button();
             this.buttonUsers = new System.Windows.Forms.Button();
             this.buttonEmploye = new System.Windows.Forms.Button();
@@ -41,21 +48,14 @@
             this.buttonProprietaires = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonBiens = new System.Windows.Forms.Button();
-            this.BarreTitulaire = new System.Windows.Forms.Panel();
-            this.btnMinimized = new System.Windows.Forms.PictureBox();
-            this.btnMaximized = new System.Windows.Forms.PictureBox();
-            this.btnRestaure = new System.Windows.Forms.PictureBox();
-            this.btnClose = new System.Windows.Forms.PictureBox();
-            this.btnSlideBar = new System.Windows.Forms.PictureBox();
-            this.panelContainer = new System.Windows.Forms.Panel();
             this.MenuVertical.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.BarreTitulaire.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMaximized)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnRestaure)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnSlideBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // MenuVertical
@@ -77,6 +77,95 @@
             this.MenuVertical.Name = "MenuVertical";
             this.MenuVertical.Size = new System.Drawing.Size(265, 650);
             this.MenuVertical.TabIndex = 0;
+            // 
+            // BarreTitulaire
+            // 
+            this.BarreTitulaire.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BarreTitulaire.Controls.Add(this.btnMinimized);
+            this.BarreTitulaire.Controls.Add(this.btnMaximized);
+            this.BarreTitulaire.Controls.Add(this.btnRestaure);
+            this.BarreTitulaire.Controls.Add(this.btnClose);
+            this.BarreTitulaire.Controls.Add(this.btnSlideBar);
+            this.BarreTitulaire.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BarreTitulaire.Location = new System.Drawing.Point(265, 0);
+            this.BarreTitulaire.Name = "BarreTitulaire";
+            this.BarreTitulaire.Size = new System.Drawing.Size(759, 50);
+            this.BarreTitulaire.TabIndex = 1;
+            this.BarreTitulaire.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BarreTitulaire_MouseDown);
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(265, 50);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(759, 600);
+            this.panelContainer.TabIndex = 2;
+            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
+            // 
+            // btnMinimized
+            // 
+            this.btnMinimized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimized.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinimized.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimized.Image")));
+            this.btnMinimized.Location = new System.Drawing.Point(677, 8);
+            this.btnMinimized.Name = "btnMinimized";
+            this.btnMinimized.Size = new System.Drawing.Size(18, 18);
+            this.btnMinimized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMinimized.TabIndex = 4;
+            this.btnMinimized.TabStop = false;
+            this.btnMinimized.Click += new System.EventHandler(this.btnMinimized_Click);
+            // 
+            // btnMaximized
+            // 
+            this.btnMaximized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximized.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMaximized.Image = ((System.Drawing.Image)(resources.GetObject("btnMaximized.Image")));
+            this.btnMaximized.Location = new System.Drawing.Point(701, 8);
+            this.btnMaximized.Name = "btnMaximized";
+            this.btnMaximized.Size = new System.Drawing.Size(20, 20);
+            this.btnMaximized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMaximized.TabIndex = 3;
+            this.btnMaximized.TabStop = false;
+            this.btnMaximized.Click += new System.EventHandler(this.btnMaximized_Click);
+            // 
+            // btnRestaure
+            // 
+            this.btnRestaure.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestaure.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestaure.Image = ((System.Drawing.Image)(resources.GetObject("btnRestaure.Image")));
+            this.btnRestaure.Location = new System.Drawing.Point(701, 8);
+            this.btnRestaure.Name = "btnRestaure";
+            this.btnRestaure.Size = new System.Drawing.Size(18, 18);
+            this.btnRestaure.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnRestaure.TabIndex = 2;
+            this.btnRestaure.TabStop = false;
+            this.btnRestaure.Visible = false;
+            this.btnRestaure.Click += new System.EventHandler(this.btnRestaure_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.Location = new System.Drawing.Point(727, 8);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(18, 18);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnClose.TabIndex = 1;
+            this.btnClose.TabStop = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSlideBar
+            // 
+            this.btnSlideBar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSlideBar.Image = ((System.Drawing.Image)(resources.GetObject("btnSlideBar.Image")));
+            this.btnSlideBar.Location = new System.Drawing.Point(5, 7);
+            this.btnSlideBar.Name = "btnSlideBar";
+            this.btnSlideBar.Size = new System.Drawing.Size(35, 35);
+            this.btnSlideBar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnSlideBar.TabIndex = 0;
+            this.btnSlideBar.TabStop = false;
+            this.btnSlideBar.Click += new System.EventHandler(this.btnSlideBar_Click);
             // 
             // buttonSettings
             // 
@@ -182,13 +271,13 @@
             this.buttonDocuments.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDocuments.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonDocuments.ForeColor = System.Drawing.Color.White;
-            this.buttonDocuments.Image = ((System.Drawing.Image)(resources.GetObject("buttonDocuments.Image")));
+            this.buttonDocuments.Image = global::SYNDIC_1._0.Properties.Resources.residenceDone1;
             this.buttonDocuments.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.buttonDocuments.Location = new System.Drawing.Point(0, 234);
             this.buttonDocuments.Name = "buttonDocuments";
             this.buttonDocuments.Size = new System.Drawing.Size(265, 45);
             this.buttonDocuments.TabIndex = 4;
-            this.buttonDocuments.Text = "Documents";
+            this.buttonDocuments.Text = "Résidence";
             this.buttonDocuments.UseVisualStyleBackColor = true;
             this.buttonDocuments.Click += new System.EventHandler(this.buttonDocuments_Click);
             // 
@@ -251,10 +340,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::SYNDIC_1._0.Properties.Resources.logoSyndicBlueFoncé;
+            this.pictureBox1.Image = global::SYNDIC_1._0.Properties.Resources.LOGOtest8;
             this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(273, 76);
+            this.pictureBox1.Size = new System.Drawing.Size(267, 76);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -278,95 +367,6 @@
             this.buttonBiens.UseVisualStyleBackColor = true;
             this.buttonBiens.Click += new System.EventHandler(this.buttonBiens_Click);
             // 
-            // BarreTitulaire
-            // 
-            this.BarreTitulaire.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.BarreTitulaire.Controls.Add(this.btnMinimized);
-            this.BarreTitulaire.Controls.Add(this.btnMaximized);
-            this.BarreTitulaire.Controls.Add(this.btnRestaure);
-            this.BarreTitulaire.Controls.Add(this.btnClose);
-            this.BarreTitulaire.Controls.Add(this.btnSlideBar);
-            this.BarreTitulaire.Dock = System.Windows.Forms.DockStyle.Top;
-            this.BarreTitulaire.Location = new System.Drawing.Point(265, 0);
-            this.BarreTitulaire.Name = "BarreTitulaire";
-            this.BarreTitulaire.Size = new System.Drawing.Size(759, 50);
-            this.BarreTitulaire.TabIndex = 1;
-            this.BarreTitulaire.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BarreTitulaire_MouseDown);
-            // 
-            // btnMinimized
-            // 
-            this.btnMinimized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMinimized.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnMinimized.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimized.Image")));
-            this.btnMinimized.Location = new System.Drawing.Point(677, 8);
-            this.btnMinimized.Name = "btnMinimized";
-            this.btnMinimized.Size = new System.Drawing.Size(18, 18);
-            this.btnMinimized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btnMinimized.TabIndex = 4;
-            this.btnMinimized.TabStop = false;
-            this.btnMinimized.Click += new System.EventHandler(this.btnMinimized_Click);
-            // 
-            // btnMaximized
-            // 
-            this.btnMaximized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMaximized.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnMaximized.Image = ((System.Drawing.Image)(resources.GetObject("btnMaximized.Image")));
-            this.btnMaximized.Location = new System.Drawing.Point(701, 8);
-            this.btnMaximized.Name = "btnMaximized";
-            this.btnMaximized.Size = new System.Drawing.Size(20, 20);
-            this.btnMaximized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btnMaximized.TabIndex = 3;
-            this.btnMaximized.TabStop = false;
-            this.btnMaximized.Click += new System.EventHandler(this.btnMaximized_Click);
-            // 
-            // btnRestaure
-            // 
-            this.btnRestaure.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRestaure.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnRestaure.Image = ((System.Drawing.Image)(resources.GetObject("btnRestaure.Image")));
-            this.btnRestaure.Location = new System.Drawing.Point(701, 8);
-            this.btnRestaure.Name = "btnRestaure";
-            this.btnRestaure.Size = new System.Drawing.Size(18, 18);
-            this.btnRestaure.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btnRestaure.TabIndex = 2;
-            this.btnRestaure.TabStop = false;
-            this.btnRestaure.Visible = false;
-            this.btnRestaure.Click += new System.EventHandler(this.btnRestaure_Click);
-            // 
-            // btnClose
-            // 
-            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
-            this.btnClose.Location = new System.Drawing.Point(727, 8);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(18, 18);
-            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btnClose.TabIndex = 1;
-            this.btnClose.TabStop = false;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
-            // btnSlideBar
-            // 
-            this.btnSlideBar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnSlideBar.Image = ((System.Drawing.Image)(resources.GetObject("btnSlideBar.Image")));
-            this.btnSlideBar.Location = new System.Drawing.Point(5, 7);
-            this.btnSlideBar.Name = "btnSlideBar";
-            this.btnSlideBar.Size = new System.Drawing.Size(35, 35);
-            this.btnSlideBar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btnSlideBar.TabIndex = 0;
-            this.btnSlideBar.TabStop = false;
-            this.btnSlideBar.Click += new System.EventHandler(this.btnSlideBar_Click);
-            // 
-            // panelContainer
-            // 
-            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContainer.Location = new System.Drawing.Point(265, 50);
-            this.panelContainer.Name = "panelContainer";
-            this.panelContainer.Size = new System.Drawing.Size(759, 600);
-            this.panelContainer.TabIndex = 2;
-            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
-            // 
             // frmMenuOfficiel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -380,13 +380,13 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MenuOfficiel";
             this.MenuVertical.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.BarreTitulaire.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMaximized)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnRestaure)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnSlideBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
